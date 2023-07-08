3f7cf84a828dd5b72244d328dcee3bb1e8016238
Samarpita Roy <samarpitaroy01@gmail.com>
2023-07-08 12:17:20 +0530

    The Dice Game!

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -9,12 +9,15 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    // IB Outlet allows me to reference the UI elements
+    @IBOutlet weak var diceOne: UIImageView!
+    @IBOutlet weak var diceTwo: UIImageView!
+    
+    // IB Action will be triggered when an action occurs on this particular UI element
+    @IBAction func rollTheDice(_ sender: UIButton) {
+        
+        let diceArray = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")] // let instead of var when immutable
+        diceOne.image = diceArray.randomElement() // randomisation of dice images in Swift
+        diceTwo.image = diceArray.randomElement() // same
     }
-
-
 }
-
